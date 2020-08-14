7381dc721797f61f3055252727ac0acbda33ca41
tajahouse <ahlliahtaja@yahoo.com>
2020-08-13 17:15:45 -0700

    Setting up testing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,9 @@
 const enhancer = require('./enhancer.js');
+const { exportAllDeclaration } = require('babel-types');
+const { executionAsyncId } = require('async_hooks');
+const { italic } = require('chalk');
+const { success, repair, fail, get } = require('../enhancing/enhancer')
 // test away!
+test(" Testing my test 😃 ", () =>{
+    expect(1+1).toBe(2)
+})  
